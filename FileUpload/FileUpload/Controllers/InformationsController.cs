8b0279acdfc7875d24e8a4b42ff489b22aef8c89
fashraf186 <fashraf@xavor.com>
2018-11-30 12:52:59 +0500

    1. Store Image in the Database using varbinary(max) binary format 2. Retrieve Image from the Database and convert binary format into jpg.

---
+++ FileUpload/FileUpload/Controllers/InformationsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FileUpload.Models;
+
+namespace FileUpload.Controllers
+{
+    public class InformationsController : Controller
+    {
+        private ProjectDataEntities db = new ProjectDataEntities();
+
+        // GET: Informations
+        public ActionResult Index()
+        {
+            return View(db.Information.ToList());
+        }
+
+        // GET: Informations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Information information = db.Information.Find(id);
+            if (information == null)
+            {
+                return HttpNotFound();
+            }
+            return View(information);
+        }
+
+        // GET: Informations/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Informations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PersonID,FirstName,LastName")] Information information, HttpPostedFileBase File1)   //HTTPPostedFilebase is use to post file in the database
+        {
+            if (ModelState.IsValid)
+            {
+                if (File1 != null && File1.ContentLength > 0)
+                {
+                    information.ProfilePic = new byte[File1.ContentLength]; // file1 to store image in binary formate  
+                    File1.InputStream.Read(information.ProfilePic, 0, File1.ContentLength);
+                    db.Information.Add(information);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+               
+            }
+
+            return View(information);
+        }
+
+        // GET: Informations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Information information = db.Information.Find(id);
+            if (information == null)
+            {
+                return HttpNotFound();
+            }
+            return View(information);
+        }
+
+        // POST: Informations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PersonID,FirstName,LastName,ProfilePic,Path")] Information information)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(information).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(information);
+        }
+
+        // GET: Informations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Information information = db.Information.Find(id);
+            if (information == null)
+            {
+                return HttpNotFound();
+            }
+            return View(information);
+        }
+
+        // POST: Informations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Information information = db.Information.Find(id);
+            db.Information.Remove(information);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
